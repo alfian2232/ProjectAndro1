79fb605a6bad24e68882b6580f448d59b38a0868
Alfian Karim Fathur Rahman <54885070+alfian2232@users.noreply.github.com>
2020-11-17 21:49:09 +0700

    update_tambah_produk

--- Pict/app/src/main/java/com/example/asus/pict/Petani/HalamanUtamaActivity.java
+++ Pict/app/src/main/java/com/example/asus/pict/Petani/HalamanUtamaActivity.java
@@ -1,15 +1,32 @@
 package com.example.asus.pict.Petani;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.CardView;
+import android.view.View;
 
 import com.example.asus.pict.R;
 
 public class HalamanUtamaActivity extends AppCompatActivity {
 
+    CardView cv_produk,cv_tambah,cv_pengaturan_akun;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_halaman_utama);
+        cv_produk = findViewById(R.id.cv_produk);
+        cv_tambah = findViewById(R.id.cv_tambah);
+        cv_pengaturan_akun = findViewById(R.id.cv_pengaturan_akun);
+
+        cv_produk.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(HalamanUtamaActivity.this,ProdukSayaActivity.class));
+            }
+        });
+
+        
+
     }
 }
--- Pict/app/src/main/java/com/example/asus/pict/Petani/ProdukSayaActivity.java
+++ Pict/app/src/main/java/com/example/asus/pict/Petani/ProdukSayaActivity.java
@@ -2,14 +2,23 @@ package com.example.asus.pict.Petani;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 import com.example.asus.pict.R;
 
+import java.util.Objects;
+
 public class ProdukSayaActivity extends AppCompatActivity {
 
+    Toolbar toolbar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_produk_saya);
+
+        toolbar = findViewById(R.id.toolbar);
+
+        setSupportActionBar(toolbar);
+        Objects.requireNonNull(getSupportActionBar()).setDisplayHomeAsUpEnabled(true);
     }
 }
