bd2edd85a418ea82c1f783598638212b033232c1
anas87k <khoirul.anas.id@gmail.com>
2020-11-19 14:55:13 +0700

    pembeli update

---
+++ Pict/app/src/main/java/com/example/asus/pict/pembeli/PasswordPembeliActivity.java
@@ -0,0 +1,15 @@
+package com.example.asus.pict.pembeli;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.asus.pict.R;
+
+public class PasswordPembeliActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_password_pembeli);
+    }
+}
--- Pict/app/src/main/java/com/example/asus/pict/pembeli/PembeliMainActivity.java
+++ Pict/app/src/main/java/com/example/asus/pict/pembeli/PembeliMainActivity.java
@@ -1,15 +1,53 @@
 package com.example.asus.pict.pembeli;
 
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 
+import com.example.asus.pict.Fragment_Home;
+import com.example.asus.pict.Fragment_Profile;
+import com.example.asus.pict.Fragment_Search;
 import com.example.asus.pict.R;
 
-public class PembeliMainActivity extends AppCompatActivity {
+public class PembeliMainActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pembeli_main);
+        loadFragment(new Fragment_Home());
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bn_main);
+        bottomNavigationView.setOnNavigationItemSelectedListener(this);
+    }
+
+    private boolean loadFragment(Fragment fragment){
+        if (fragment != null){
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.fl_container, fragment)
+                    .commit();
+            return true;
+        }
+        return false;
+    }
+
+    public boolean onNavigationItemSelected(@NonNull MenuItem item){
+        Fragment fragment = null;
+        switch(item.getItemId()){
+            case R.id.id_home:
+                fragment = new Fragment_Home();
+                break;
+            case R.id.id_search:
+                fragment = new Fragment_Search();
+                break;
+            case  R.id.id_profile:
+                fragment = new Fragment_Profile();
+                break;
+        }
+        return loadFragment(fragment);
     }
 }
