8b95cf793536dc621752408e035cd85c87cd90fc
Alfian Karim Fathur Rahman <54885070+alfian2232@users.noreply.github.com>
2020-11-24 23:09:13 +0700

    update kembali

--- Pict/app/src/main/java/com/example/asus/pict/pembeli/PembeliMainActivity.java
+++ Pict/app/src/main/java/com/example/asus/pict/pembeli/PembeliMainActivity.java
@@ -3,6 +3,7 @@ package com.example.asus.pict.pembeli;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MenuItem;
@@ -13,12 +14,21 @@ import com.example.asus.pict.Fragment_Search;
 import com.example.asus.pict.R;
 
 public class PembeliMainActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener {
-
+    BottomNavigationView nav_pembeli;
+    FragmentManager fragmentManager;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pembeli_main);
-        loadFragment(new Fragment_Home());
+        nav_pembeli = findViewById(R.id.nav_pembeli);
+
+            nav_pembeli.setSelectedItemId(R.id.p_home);
+            Fragment_Home f_home = new Fragment_Home();
+            fragmentManager = getSupportFragmentManager();
+            fragmentManager
+                    .beginTransaction()
+                    .replace(R.id.fl_container, f_home).addToBackStack(null)
+                    .commit();
 
         BottomNavigationView bottomNavigationView = findViewById(R.id.nav_pembeli);
         bottomNavigationView.setOnNavigationItemSelectedListener(this);
@@ -26,7 +36,8 @@ public class PembeliMainActivity extends AppCompatActivity implements BottomNavi
 
     private boolean loadFragment(Fragment fragment){
         if (fragment != null){
-            getSupportFragmentManager()
+            fragmentManager = getSupportFragmentManager();
+            fragmentManager
                     .beginTransaction()
                     .replace(R.id.fl_container, fragment)
                     .commit();
